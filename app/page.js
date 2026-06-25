a22f56e6945233dfd127f72165d23f582cc661fb
rafaelmot7 <106933468+rafaelmot7@users.noreply.github.com>
2026-06-24 21:22:04 -0300

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home(){
-return (
-<main>
-<h1>⚽ Palpite Campeão</h1>
-<p>Bolão Copa ao vivo</p>
-<a href="/login">Cadastro</a>
-</main>
-)
-}
--- app/ranking/page.js
+++
@@ -1,8 +0,0 @@
-export default function Ranking(){
-return (
-<main>
-<h1>🏆 Ranking</h1>
-<p>João - 120 pontos</p>
-</main>
-)
-}
